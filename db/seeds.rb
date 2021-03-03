7f76cd7ebb50f692900a091ab93fb28ed05a8952
Trey Kemp <kemptrey@outlook.com>
2021-03-03 14:32:40 -0700

    added axios

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "faker"
+
+catagories = ["food", "travel", "misc"]
+
+10.times do |i|
+  Book.create(
+    title: Faker::Hacker.abbreviation,
+    author: Faker::Hacker.say_something_smart,
+  )
+end
